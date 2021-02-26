0e09fdd2b2c51f921fa0a7261749a5d8cfc44685
Yerasyl <yerasyl.amanbek@astanait.edu.kz>
2021-02-26 17:09:49 +0600

    Added LoginRepositry, progress: 10%

--- src/com/company/PostgreDB.java
+++ src/com/company/PostgreDB.java
@@ -8,3 +8,12 @@ public class PostgreDB {
         return false;
     }
 }
+
+class LoginRepository {
+    private String user_name;
+    private String pass;
+    public boolean loginToAccount(String user_name, String pass){
+        //todo implement logic that returns true
+        return false;
+    }
+}
